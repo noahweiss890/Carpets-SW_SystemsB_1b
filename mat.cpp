f0ed3a505871f44f003b6be4bc663f5b79094f41
noahweiss890 <93292627+noahweiss890@users.noreply.github.com>
2022-03-14 22:13:28 +0200

    Update mat.cpp

--- mat.cpp
+++ mat.cpp
@@ -5,8 +5,8 @@ using namespace std;
 #include "mat.hpp"
 
 namespace ariel {
-    static const int min_char = 33;
-    static const int max_char = 126;
+    static const int min_char = 33; // the lowest char that can be inputted
+    static const int max_char = 126; // the highest char that can be inputted
 
 	string mat(int col, int row, char symb1, char symb2) {
 		if(col < 0 || col%2 != 1 || row < 0 || row%2 != 1 || symb1 < min_char || symb1 > max_char || symb2 < min_char || symb2 > max_char) {
@@ -18,19 +18,19 @@ namespace ariel {
 		
 		// paint every other square with symb2
         for(int k = 1; k <= col/2 && k <= row/2; k += 2) {
-            // top and bottom line
+            // draw top and bottom line with symb2
             for(int i = k; i < col - k; i++) {
                 vect[k][i] = symb2;
                 vect[row-k-1][i] = symb2;
             }
-            // left and right side
+            // draw left and right side with symb2
             for(int i = k; i < row - k; i++) {
                 vect[i][k] = symb2;
                 vect[i][col-k-1] = symb2;
             }
         }
         
-        string ans;
+        string ans; // will hold the string to be returned
         
         // create a string from the vector
         for(int i = 0; i < row; i++) {
