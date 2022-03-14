c031f424652f7566556a031c5a182006323ef52b
noahweiss890 <nweiss890@gmail.com>
2022-03-14 16:40:14 +0200

    func works

--- mat.cpp
+++ mat.cpp
@@ -5,17 +5,18 @@ using namespace std;
 #include "mat.hpp"
 
 namespace ariel {
-    static const int minimum = 33;
-    static const int maximum = 126;
+    static const int min_char = 33;
+    static const int max_char = 126;
 
 	string mat(int col, int row, char symb1, char symb2) {
-		if(col < 0 || col%2 != 1 || row < 0 || row%2 != 1 || symb1 < minimum || symb1 > maximum || symb2 < minimum || symb2 > maximum) {
+		if(col < 0 || col%2 != 1 || row < 0 || row%2 != 1 || symb1 < min_char || symb1 > max_char || symb2 < min_char || symb2 > max_char) {
             throw invalid_argument("Mat size is always odd!");
         }
 
+        // initiallize the vector and fill with symb1
         vector<vector<char>> vect(row, vector<char>(col, symb1));
 		
-		// paint every other ring with symb2
+		// paint every other square with symb2
         for(int k = 1; k <= col/2 && k <= row/2; k += 2) {
             // top and bottom line
             for(int i = k; i < col - k; i++) {
