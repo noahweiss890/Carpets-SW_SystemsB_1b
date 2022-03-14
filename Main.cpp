fc7602c63347c5ec94c178952a6548873b7360f4
noahweiss890 <nweiss890@gmail.com>
2022-03-14 17:07:32 +0200

    adding main

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include "mat.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to the mat shop!" << endl;
+    cout << "Here you can make custom mats." << endl;
+
+    int row;
+    cout << "Enter height of mat: ";
+    cin >> row;
+    cout << endl;
+
+    int col;
+    cout << "Enter width of mat: ";
+    cin >> col;
+    cout << endl;
+
+    char symb1;
+    cout << "Enter first char design: ";
+    cin >> symb1;
+    cout << endl;
+
+    char symb2;
+    cout << "Enter second char design: ";
+    cin >> symb2;
+    cout << endl;
+
+    cout << "Making your custom mat..." << endl;
+    cout << ariel::mat(col, row, symb1, symb2) << endl;
+
+    cout << endl << "Come back soon!";
+
+    return 0;
+}
